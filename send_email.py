2f5c46b86929d28d0ea8a28cca61d9c2fabbafb6
Clarke <clarkevandenhoven@gmail.com>
2018-10-25 22:47:25 -0400

    Added .gitignore file and changes
    Removed silly files

--- send_email.py
+++ send_email.py
@@ -1,8 +1,28 @@
 import smtplib
+from wikipedia_requests import parse_request
+from datetime import datetime
+from config.py import *
 
-try:
-    server =  smtplib.SMTP("smtp.gmail.com", 587)
-    server.ehlo()
-    server.starttls()
-except:
-    print("Something went wrong...")
+today = datetime.today()
+day, month, year = today.day, today.month, today.year
+date = str(day) + "/" + str(month) + "/" + str(year)
+article = parse_request()
+
+sent_from = user
+to = "clarkevandenhoven@gmail.com"
+subject = "Article for " + date + " : " +  article["title"]
+body = article["summary"] + "\n\nRead more at: " + article["url"] + "\n\n" + article["image"]
+
+email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sent_from, to, subject, body)
+
+server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+server.ehlo()
+server.login(user, password)
+server.sendmail(sent_from, to, email_text.encode('utf-8'))
+server.close()
--- wikipedia_requests.py
+++ wikipedia_requests.py
@@ -19,11 +19,22 @@ def make_request():
     r = requests.get(build_feed_request())
     return(r)
 
-# TFA is the code that wikipedia gives to today's featured article
-featured_article = make_request().json()["tfa"]
+def parse_request():
+    r = make_request()
+    
+    # TFA is the code that wikipedia gives to today's featured article
+    featured_article = make_request().json()["tfa"]
+    
+    title = featured_article["title"].replace('_', ' ')
+    image = featured_article["thumbnail"]["source"]
+    # Be sure to replace non breaking spaces with real spaces
+    summary = featured_article["extract"].replace('\xa0', ' ')
+    url = featured_article["content_urls"]["desktop"]["page"]
 
-title = featured_article["displaytitle"]
-image = featured_article["thumbnail"]["source"]
-summary = featured_article["extract"]
-
-
+    article_data = {
+            "title" : title,
+            "image" : image,
+            "summary" : summary,
+            "url" : url
+            }
+    return(article_data)
